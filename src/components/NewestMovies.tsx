b7c0cd7916bca3f811cb6dcb0d9770ad4b5861bc
Shady Hussein <shadyhussein99@gmail.com>
2023-05-01 18:39:42 +0200

    Structuring Newest movies section

--- src/components/NewestMovies.tsx
+++ src/components/NewestMovies.tsx
@@ -2,9 +2,17 @@ import { useState, useEffect } from "react"
 
 function NewestMovies() {
 
+    const [newestMovies, setNewestMovies] = useState<string[]>([])
+
+    useEffect(() => {
+        fetch("https://api.themoviedb.org/3/discover/movie?api_key=9642c68d4c399a387d2f64c0680f0bc7&language=en-US&sort_by=popularity.desc&page=1")
+            .then(res => res.json())
+            .then(res => setNewestMovies(res))
+            .catch(err => console.log(err))
+    }, [])
 
     return <section>
-        <h2>Newest Movies</h2>
+        <h2>What's New</h2>
     </section>
 }
 
