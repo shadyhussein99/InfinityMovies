6eafed0676dd6b157848ccba4c7daf5d6599732e
Shady Hussein <shadyhussein99@gmail.com>
2023-05-03 00:46:26 +0200

    Constructing movie details page

--- src/components/MovieDetails.tsx
+++ src/components/MovieDetails.tsx
@@ -2,10 +2,20 @@ import { useSelector } from "react-redux";
 
 function MovieDetails() {
     const movie = useSelector((state: any) => state.search.value);
-    const [ , rottenTomatoes] = movie.Ratings
+    const [, rottenTomatoes] = movie.Ratings
 
     return (
-        <main>{movie.Title}</main>
+        <main>
+            <img src={movie?.Poster} alt="movie-poster" />
+            <i className="fa-solid fa-arrow-left"></i>
+
+            <div>
+                <span>IMDB {movie?.imdbRating}</span>
+                <span>({movie?.imdbVotes}) reviews</span>
+            </div>
+            <h1>{movie?.Title}</h1>
+            <p>{movie?.Genre}</p>
+        </main>
     );
 }
 
