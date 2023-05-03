52cfcd39c9297246527a620e4c080648eea7d2d7
Shady Hussein <shadyhussein99@gmail.com>
2023-05-03 03:03:50 +0200

    Handling response on searching for unValid movie

--- src/components/MovieDetails.tsx
+++ src/components/MovieDetails.tsx
@@ -1,20 +1,32 @@
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 
 function MovieDetails() {
     const movie = useSelector((state: any) => state.search.value);
-    const [, rottenTomatoes] = movie.Ratings
+    const [rottenTomatoesRating, setRottenTomatoesRating] = useState<any>("")
+    
+    useEffect(() => {
+        if (movie.Response === "True") {
+            const [ , rottenTomatoes] = movie.Ratings;
+            setRottenTomatoesRating(rottenTomatoes.Value)
+    }}, [movie])
+    
 
-    return (
+    return ( movie.Response === "True" ?
         <main>
             <img src={movie?.Poster} alt="movie-poster" />
             <i className="fa-solid fa-arrow-left"></i>
 
             <div>
-                <span>IMDB {movie?.imdbRating}</span>
-                <span>({movie?.imdbVotes}) reviews</span>
+                <span>IMDB {movie?.imdbRating}/10</span>
+                <span>Rotten Tomatoes {rottenTomatoesRating}</span>
+                <span>({movie?.imdbVotes} reviews)</span>
             </div>
             <h1>{movie?.Title}</h1>
-            <p>{movie?.Genre}</p>
+            <div>
+                <span>{movie?.Genre}</span>
+                <span>{movie?.Runtime}</span>
+            </div>
             <p>{movie?.Plot}</p>
             <hr />
             <p>Main Actors <span>{movie?.Actors}</span></p>
@@ -23,8 +35,15 @@ function MovieDetails() {
             <hr />
             <p>Writer <span>{movie?.Writer}</span></p>
             <hr />
+            <p>Release Date <span>{movie?.Released}</span></p>
+            <hr />
             <p>Country <span>{movie?.Country}</span></p>
-        </main>
+            <hr />
+            <p>Box Office <span>{movie?.BoxOffice}</span></p>
+            <hr />
+            <p>Awards <span>{movie?.Awards}</span></p>
+        </main> :
+        <h1>No matching Movie or Series!</h1>
     );
 }
 
