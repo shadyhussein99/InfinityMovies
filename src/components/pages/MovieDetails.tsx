05def36af9822018a73dbb72d5e69de6911fd440
Shady Hussein <shadyhussein99@gmail.com>
2023-05-21 21:04:03 +0200

    styling movie details in large screens

--- src/components/pages/MovieDetails.tsx
+++ src/components/pages/MovieDetails.tsx
@@ -20,36 +20,36 @@ function MovieDetails() {
 
     return (movie.Response === "True" ?
         <main>
-            <img src={movie?.Poster} alt="movie-poster" />
-            <i className="fa-solid fa-arrow-left" onClick={() => navigate("/")}></i>
+            <img className="poster" src={movie?.Poster} alt="movie-poster" />
+            <i className="fa-solid fa-arrow-left back-arrow" onClick={() => navigate("/")}></i>
 
-            <div>
-                <span>IMDB {movie?.imdbRating}/10</span>
-                <span>({movie?.imdbVotes} reviews)</span>
-            </div>
+            <section className="details-section">
+                <span className="ratingValue-span">IMDB {movie?.imdbRating}/10</span>
+                <span className="ratingNumber-span">({movie?.imdbVotes} reviews)</span>
 
-            <h1>{movie?.Title}</h1>
+                <h1>{movie?.Title}</h1>
 
-            <div>
-                <span>{movie?.Genre}</span>
-                <span>{movie?.Runtime}</span>
-            </div>
+                <div>
+                    <span>{movie?.Genre}</span>
+                    <span>{movie?.Runtime}</span>
+                </div>
 
-            <p>{movie?.Plot}</p>
-            <hr />
-            <p>Main Actors <span>{movie?.Actors}</span></p>
-            <hr />
-            <p>Director <span>{movie?.Director}</span></p>
-            <hr />
-            <p>Writer <span>{movie?.Writer}</span></p>
-            <hr />
-            <p>Release Date <span>{movie?.Released}</span></p>
-            <hr />
-            <p>Country <span>{movie?.Country}</span></p>
-            <hr />
-            <p>Box Office <span>{movie?.BoxOffice}</span></p>
-            <hr />
-            <p>Awards <span>{movie?.Awards}</span></p>
+                <p>{movie?.Plot}</p>
+                <hr />
+                <p>Main Actors <span>{movie?.Actors}</span></p>
+                <hr />
+                <p>Director <span>{movie?.Director}</span></p>
+                <hr />
+                <p>Writer <span>{movie?.Writer}</span></p>
+                <hr />
+                <p>Release Date <span>{movie?.Released}</span></p>
+                <hr />
+                <p>Country <span>{movie?.Country}</span></p>
+                <hr />
+                <p>Box Office <span>{movie?.BoxOffice}</span></p>
+                <hr />
+                <p>Awards <span>{movie?.Awards}</span></p>
+            </section>
         </main> :
         <h1>No matching Movie or Series!</h1>
     );
